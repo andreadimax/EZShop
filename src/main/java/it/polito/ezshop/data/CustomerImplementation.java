c1af5b6462ed0b22ee3e7d7fe04035ff1921a7ba
ANDREA DI MAURO s288048 <s288048@studenti.polito.it>
2021-05-08 14:27:06 +0200

    progress: completed Customer related functions

---
+++ src/main/java/it/polito/ezshop/data/CustomerImplementation.java
@@ -0,0 +1,58 @@
+package it.polito.ezshop.data;
+
+public class CustomerImplementation implements Customer {
+    private String name;
+    private String customerCard;
+    private Integer id;
+    private Integer points;
+
+    public CustomerImplementation(String name, Integer id, Integer points, String customerCard){
+        this.id = id;
+        this.name = name;
+        this.points = points;
+        if(customerCard.matches("\\d{10}")) {
+            this.customerCard = customerCard;
+        }
+    }
+
+    @Override
+    public String getCustomerName() {
+        return name;
+    }
+
+    @Override
+    public void setCustomerName(String customerName) {
+        this.name = customerName;
+    }
+
+    @Override
+    public String getCustomerCard() {
+        return customerCard;
+    }
+
+    @Override
+    public void setCustomerCard(String customerCard) {
+        this.customerCard = customerCard;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getPoints() {
+        return points;
+    }
+
+    @Override
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+}
