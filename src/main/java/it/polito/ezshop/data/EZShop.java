608c40c132649219de7701034fda79f34b507246
Alessio Bincoletto <s234970@studenti.polito.it>
2021-05-26 18:36:34 +0200

    fixed validatecard from private to public

--- src/main/java/it/polito/ezshop/data/EZShop.java
+++ src/main/java/it/polito/ezshop/data/EZShop.java
@@ -117,7 +117,7 @@ public class EZShop implements EZShopInterface {
         return checkDigit == Character.getNumericValue(bcode[len-1]);
     }
 //////---------------------------------------------------------------------------
-    private static boolean validateCard(String cardNumber) {
+    public static boolean validateCard(String cardNumber) {
         if (cardNumber == null || cardNumber.equals("") || cardNumber.length()<2)
             return false;
         //convert to array of int
